d1bd095ac36fef41e9ea6d3d631dee20f5c11b68
Javi <mancheguet96@gmail.com>
2025-08-11 12:05:24 +0200

    first commit

---
+++ js/redirect.js
@@ -0,0 +1,10 @@
+(async () => {
+    const path = window.location.pathname.replace(/^\/+/, ''); // quita "/" inicial
+    const res = await fetch('/data/links.json');
+    const links = await res.json();
+    if (links[path]) {
+        window.location.href = links[path];
+    } else {
+        window.location.href = '/404.html'; // si no existe, manda a la página de error
+    }
+})();
