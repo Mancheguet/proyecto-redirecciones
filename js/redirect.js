bff605fd33bd67363d51651bf3f2e9a4c016fd38
Javi <mancheguet96@gmail.com>
2025-08-11 12:23:18 +0200

    Cambio de const res para quitar /

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,6 @@
 (async () => {
     const path = window.location.pathname.replace(/^\/+/, ''); // quita "/" inicial
-    const res = await fetch('/data/links.json');
+    const res = await fetch('data/links.json');
     const links = await res.json();
     if (links[path]) {
         window.location.href = links[path];
