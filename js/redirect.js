d869711138171b477a58045a5f544adda3d11b5e
Javi <mancheguet96@gmail.com>
2025-08-11 12:29:07 +0200

    añadir mensajes en consola

--- js/redirect.js
+++ js/redirect.js
@@ -1,19 +1,24 @@
 (async () => {
-    // Obtener basePath (subcarpeta o raíz)
     const pathParts = window.location.pathname.split('/').filter(Boolean);
     const basePath = pathParts.length > 0 ? '/' + pathParts[0] + '/' : '/';
-
-    // Obtener alias (lo que va después del basePath)
     const alias = pathParts.length > 1 ? pathParts.slice(1).join('/') : '';
 
+    console.log('basePath:', basePath);
+    console.log('alias:', alias);
+
     try {
+        console.log('Intentando cargar:', basePath + 'data/links.json');
         const res = await fetch(basePath + 'data/links.json');
         if (!res.ok) throw new Error('No se pudo cargar links.json');
         const links = await res.json();
 
+        console.log('Links cargados:', links);
+
         if (links[alias]) {
+            console.log('Redirigiendo a:', links[alias]);
             window.location.href = links[alias];
         } else {
+            console.log('Alias no encontrado, redirigiendo a 404');
             window.location.href = basePath + '404.html';
         }
     } catch (error) {
